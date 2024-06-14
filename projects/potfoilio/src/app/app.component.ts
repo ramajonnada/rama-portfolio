c24ba7f9e63686859abf520817bff045607415ae
ramajonnada <rama.jonnada23@gmail.com>
2024-06-14 12:59:43 +0530

    feat(app): add button for home component and saparate the toolbar component

--- projects/potfoilio/src/app/app.component.ts
+++ projects/potfoilio/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet, HomeComponent, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- projects/potfoilio/src/app/home/home.component.ts
+++ projects/potfoilio/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ToolbarComponent],
+  imports: [MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
