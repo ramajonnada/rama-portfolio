c79c6a0960aa1a8136f21152d5d263fd9d12215c
ramajonnada <rama.jonnada23@gmail.com>
2024-06-21 16:48:56 +0530

    feat(footer and docs): send mail input data using emailjs

--- projects/potfoilio/src/app/footer/footer.component.ts
+++ projects/potfoilio/src/app/footer/footer.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  invalidString: string = '';
+  year: number = new Date().getFullYear();
+  formGroup: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.min(1), Validators.max(40)]),
+    mail: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('')
+  })
+
+  ngOnInit() {
+    this.formGroup.valueChanges.subscribe(e => console.log('formgroup', e));
+  }
+
+  $$submit(e: Event): void {
+    e.preventDefault();
+    emailjs.sendForm('service_554nlg8', 'template_cjqdd4o', e.target as HTMLFormElement, 'hywfQLe_tsVeQoRh5')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        alert('Email sent successfully!');
+        this.formGroup.patchValue({ name: '', mail: '', message: '' });
+      }, (error) => {
+        console.log(error.text);
+        alert('Failed to send email. Please try again later.');
+      });
+  }
 
 }
