6539a1dda792d304201423927967ee846cecf4dc
ramajonnada <rama.jonnada23@gmail.com>
2024-06-23 19:05:18 +0530

    feat(portfolio): add styles to the side toolbsr

--- projects/potfoilio/src/app/skills/const/skills.ts
+++ projects/potfoilio/src/app/skills/const/skills.ts
@@ -2,31 +2,32 @@ export const TECHNOLOGIES = [
 	{
 		main: 'Front-end Development',
 		skills: [
-			{ logo: "fa fa-brands fa-js", name: 'Js' },
-			{ logo: 'fa-brands fa-angular', name: 'Angular' },
-			{ logo: 'fa-brands fa-react', name: 'React' },
-			{ logo: 'fa-brands fa-html5', name: 'HTML' },
-			{ logo: 'fa-brands fa-css3-alt', name: 'CSS' },
-			{ logo: 'fa-brands fa-sass', name: 'SaSS' },
-			{ logo:'fa-solid fa-laptop-code', name:'Responsive Design'}
+			{ logo: "fa fa-brands fa-js", name: 'Js', number: 80 },
+			{ logo: 'fa-brands fa-angular', name: 'Angular', number: 90 },
+			{ logo: 'fa-brands fa-react', name: 'React', number: 60 },
+			{ logo: 'fa-brands fa-html5', name: 'HTML', number: 100 },
+			{ logo: 'fa-brands fa-css3-alt', name: 'CSS', number: 100 },
+			{ logo: 'fa-brands fa-sass', name: 'SaSS', number: 90 },
+			{ logo: 'fa-solid fa-laptop-code', name: 'Responsive Design', number: 100 }
 		]
 	},
 	{
 		main: 'Back-end Development',
 		skills: [
-			{ logo: 'fa-brands fa-python', name: 'Python' },
+			{ logo: 'fa-brands fa-python', name: 'Python', number: 50 },
 		]
 	},
 	{
 		main: 'Database Management',
 		skills: [
-			{ logo: 'fa-solid fa-database', name: 'SQL' },
+			{ logo: 'fa-solid fa-database', name: 'SQL', number: 60 },
 		]
 	},
 	{
 		main: 'Other',
 		skills: [
 			{ logo: 'fa-brands fa-github', name: 'Github' },
+			{ name: 'RESTful APIs' }
 		]
 	}
 
@@ -47,3 +48,6 @@ export const TECHNOLOGIES = [
 	// { logo: '', name: '' },
 	// { logo: '', name: '' },
 ];
+
+
+
--- projects/potfoilio/src/app/skills/skills.component.ts
+++ projects/potfoilio/src/app/skills/skills.component.ts
@@ -1,22 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TECHNOLOGIES } from './const/skills';
-import { MatCardModule } from '@angular/material/card';
-import { SquareDotsComponent } from '../square-dots/square-dots.component';
 import { HeadingComponent } from '../heading/heading.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 interface TechSpec {
   main: string,
   skills: Skill[]
 }
 interface Skill {
-  logo: string;
+  logo?: string;
   name: string;
+  number?: number;
 }
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule, MatCardModule, HeadingComponent],
+  imports: [CommonModule, HeadingComponent, MatCardModule, MatProgressSpinnerModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
