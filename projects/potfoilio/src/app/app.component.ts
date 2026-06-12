2048e1cffbeb7b823f45a57c108a44d071322306
ramajonnada <rama.jonnada23@gmail.com>
2026-06-12 22:37:41 +0530

    fix(app): remove router outlet and add home page in app comp

--- projects/potfoilio/src/app/app.component.ts
+++ projects/potfoilio/src/app/app.component.ts
@@ -6,29 +6,31 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { SquareDotsComponent } from './square-dots/square-dots.component';
 import { rama } from './rama';
+import { HomeComponent } from './home/home.component';
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    ToolbarComponent,
-    FooterComponent,
-    MatSidenavModule,
-    MatButtonModule,
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-    SquareDotsComponent,
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [
+		HomeComponent,
+		ToolbarComponent,
+		FooterComponent,
+		MatSidenavModule,
+		MatButtonModule,
+		RouterOutlet,
+		RouterLink,
+		RouterLinkActive,
+		SquareDotsComponent,
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
-export class AppComponent extends rama implements rama{
-  constructor(){
-    super(4);
-    console.log(this.id);
-  }
+export class AppComponent extends rama implements rama {
+	constructor() {
+		super(4);
+		console.log(this.id);
+	}
 
 
 }
